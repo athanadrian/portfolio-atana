0611a3b73e8da872300da2bab3e07f975f29220e
athanadrian <athanadrian@gmail.com>
2021-02-23 13:49:37 +0200

    fix github repositories api

--- pages/api/v1/social/github.js
+++ pages/api/v1/social/github.js
@@ -2,17 +2,21 @@ export default async (req, res) => {
   const headers = {
     Authorization: 'Token ' + process.env.GITHUB_KEY,
   };
+  // public repositories
+  const url1 = 'https://api.github.com/users/athanadrian';
+  const response1 = await fetch(url1, { headers: headers });
+  const json1 = await response1.json();
+  const numProjects = json1.public_repos;
   // followers
-  const url = 'https://api.github.com/users/athanadrian/followers';
-  const response = await fetch(url, { headers: headers });
-  const json = await response.json();
-  const numFollowers = Object.keys(json).length;
-  // projects
+  const url3 = 'https://api.github.com/users/athanadrian/followers';
+  const response3 = await fetch(url3, { headers: headers });
+  const json3 = await response3.json();
+  const numFollowers = Object.keys(json3).length;
+  // forks
   const url2 = 'https://api.github.com/users/athanadrian/repos';
   const response2 = await fetch(url2, { headers: headers });
   const json2 = await response2.json();
-  const numProjects = Object.keys(json2).length;
-  // forks
+  //const numProjects = Object.keys(json2).length;
   var forksCount = 0;
   json2.forEach((f) => {
     forksCount += f.forks_count;
