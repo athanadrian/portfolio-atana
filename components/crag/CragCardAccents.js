c517c6e461471751ccc3709c5bf3788b226b6ae7
athanadrian <athanadrian@gmail.com>
2021-04-05 17:43:49 +0300

    crag update

--- components/crag/CragCardAccents.js
+++ components/crag/CragCardAccents.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Card, CardTitle, CardText } from 'reactstrap';
 
 const CragCardAccents = () => {
-  const total = 2042;
-  const successful = 1688;
+  const total = 2125;
+  const successful = 1767;
+  const percentage = (successful * 100) / total;
   return (
     <div>
       <Card style={{ padding: '5px' }} className='shadow-border'>
-        <CardTitle>Crag Assents</CardTitle>
+        <CardTitle>Crag Assents {percentage} %</CardTitle>
         <CardText style={{ fontSize: '30px' }}>
           {total} -{' '}
           <span style={{ fontSize: '30px', color: 'teal' }}>{successful}</span>{' '}
