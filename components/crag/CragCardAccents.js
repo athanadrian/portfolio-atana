20f2f7cab21d61fa93f5f62a805cdedd65c888b6
athanadrian <athanadrian@gmail.com>
2021-06-09 14:17:13 +0300

    crag updates

--- components/crag/CragCardAccents.js
+++ components/crag/CragCardAccents.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Card, CardTitle, CardText } from 'reactstrap';
 
 const CragCardAccents = () => {
-  const total = 2186;
-  const successful = 1809;
+  const total = 2294;
+  const successful = 1900;
   const percentage = (successful * 100) / total;
   return (
     <div>
